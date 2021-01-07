55556cb6932e8c83a6ee261e095b5e9c3834814c
tarik-guit <tarikkh1993@gmail.com>
2021-01-07 18:49:47 +0100

    by tarik

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChoixComponent } from './choix/choix.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoixComponent
   ],
   imports: [
     BrowserModule,
