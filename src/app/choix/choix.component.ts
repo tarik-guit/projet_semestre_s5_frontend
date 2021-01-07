c3b2a0f940a77cfbc558bfe978d217092669b2ab
tarik-guit <tarikkh1993@gmail.com>
2021-01-07 18:53:45 +0100

    by tarik

---
+++ src/app/choix/choix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choix',
+  templateUrl: './choix.component.html',
+  styleUrls: ['./choix.component.css']
+})
+export class ChoixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
